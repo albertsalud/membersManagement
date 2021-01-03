b1d30650965938f5439359d934cdeac1ce06d77b
Albert Salud <albert.saludgarcia@cegedim.com>
2021-01-03 15:43:54 +0100

    Correccion de imports

--- src/main/java/com/albertsalud/members/controllers/admin/AdminActivityController.java
+++ src/main/java/com/albertsalud/members/controllers/admin/AdminActivityController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
